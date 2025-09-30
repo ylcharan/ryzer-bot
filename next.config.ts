8149a4fd68f49e6d353fcc4c59b9661cfb1dbf6f
ylcharan <ylcharan99@gmail.com>
2025-09-30 14:18:32 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core", "chrome-aws-lambda"],
+  },
 };
 
 export default nextConfig;
